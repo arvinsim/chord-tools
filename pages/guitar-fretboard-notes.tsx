797824be31b54145aee3ed1062067a7be582b4ae
Arthur Vincent Simon <developarvin@gmail.com>
2022-07-17 15:52:34 +0800

    Create initial fretboard

--- pages/guitar-fretboard-notes.tsx
+++ pages/guitar-fretboard-notes.tsx
@@ -1,7 +1,40 @@
 import React from "react";
 
 const GuitarFretboardNotes: NextPageWithLayout = () => {
-  return <div>Guitar Fretboard Notes</div>;
+  const notes = [
+    ["E", "F", "F#", "G", "G#", "A", "A#", "B", "C", "C#", "D", "D#", "E"],
+    ["B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"],
+    ["G", "G#", "A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G"],
+    ["D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B", "C", "C#", "D"],
+    ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A"],
+    ["E", "F", "F#", "G", "G#", "A", "A#", "B", "C", "C#", "D", "D#", "E"],
+  ];
+
+  return (
+    <div>
+      <h1>Guitar Fretboard Notes</h1>
+      <button>Reset</button>
+
+      <div>
+        {notes.map((line, lineIndex) => {
+          return (
+            <div key={lineIndex} className="grid grid-cols-12 gap-0">
+              {line.map((note) => {
+                return (
+                  <input
+                    type={"text"}
+                    value={note}
+                    key={note}
+                    className={"w-12 rounded-md"}
+                  />
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default GuitarFretboardNotes;
